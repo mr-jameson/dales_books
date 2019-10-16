d6b1dae3dedbe13da6ffd7ac3bb30f82b8c2af1b
mr-jameson <elliot.jameson4@gmail.com>
2019-10-16 11:51:00 +1100

    added databases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_235815) do
+ActiveRecord::Schema.define(version: 2019_10_16_004312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2019_10_15_235815) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "genre"
     t.bigint "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
