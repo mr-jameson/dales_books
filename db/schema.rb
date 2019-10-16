2a3a2039c1f2057e2b9ae018ac803ef3e8491c26
mr-jameson <elliot.jameson4@gmail.com>
2019-10-16 12:41:58 +1100

    added polymorphic content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_004312) do
+ActiveRecord::Schema.define(version: 2019_10_16_013122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,33 @@ ActiveRecord::Schema.define(version: 2019_10_16_004312) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "books_genres", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_books_genres_on_book_id"
+    t.index ["genre_id"], name: "index_books_genres_on_genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "imageable_type", null: false
+    t.bigint "imageable_id", null: false
+    t.string "url"
+    t.date "date_taken"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
+  end
+
   add_foreign_key "addresses", "authors"
   add_foreign_key "books", "authors"
+  add_foreign_key "books_genres", "books"
+  add_foreign_key "books_genres", "genres"
 end
